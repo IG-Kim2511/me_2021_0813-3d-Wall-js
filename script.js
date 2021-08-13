84e67becb8d27cbe72780bf27b321a982cfa6476
iikim <iikim2511@gmail.com>
2021-08-13 12:51:24 -0400

    js : scroll, z-moving

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 "use strict";
+
+( function() {
+
+    const stageElem = document.querySelector('.stage');
+    const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress-bar');
+    const selectCharacterElem = document.querySelector('.select-character');
+
+    /* js 10 */
+    let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    console.log(maxScrollValue);
+
+
+    //🍀 scroll, z-moving
+    window.addEventListener('scroll', function() {
+
+    const scrollPer = pageYOffset/ maxScrollValue; /* js 10 */    
+
+    const zMove = scrollPer * 980 -490;
+ 
+    houseElem.style.transform = `translateZ(${zMove}vw)`; /* js 20 */
+    });
+
+    // 🍀 js 30 progress bar
+
+    barElem.style.width =`${scrollPer*100}%`;
+
+})();
+
